2aed0dce03e5b1baa5fa6c86e5524081745a2587
AdamWon <ehdgus5500@gmail.com>
2021-08-01 15:01:43 +0900

    Pinball 프로젝트 시작

---
+++ Pinball/Source/Pinball/PinballGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PinballGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINBALL_API APinballGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
