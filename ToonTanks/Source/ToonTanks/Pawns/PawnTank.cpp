92a9d606aabecb0040fc55ffcdb9353975f2b81c
원동현 <ehdgus5500@gmail.com>
2021-10-04 18:20:42 +0900

    158 ~ 165
    158 Project Intro ~ 165 Exposing Variables

---
+++ ToonTanks/Source/ToonTanks/Pawns/PawnTank.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+
+APawnTank::APawnTank()
+{
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArmComp);
+}
+
+// Called when the game starts or when spawned
+void APawnTank::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void APawnTank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	Move();
+	Rotate();
+}
+
+// Called to bind functionality to input
+void APawnTank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ThisClass::CalculateMoveInput);
+	PlayerInputComponent->BindAxis("Turn", this, &ThisClass::CalculateRotateInput);
+
+}
+
+void APawnTank::CalculateMoveInput(float Value)
+{
+	MoveDirection = FVector(Value * MoveSpeed * GetWorld()->DeltaTimeSeconds, 0, 0);
+}
+
+void APawnTank::CalculateRotateInput(float Value)
+{
+	float RotateAmount = Value * RotateSpeed * GetWorld()->DeltaTimeSeconds;
+
+	FRotator Rotation = FRotator(0.0f, RotateAmount, 0.0f);
+	RotationDirection = FQuat(Rotation);
+}
+
+void APawnTank::Move()
+{
+	AddActorLocalOffset(MoveDirection, true);
+}
+
+void APawnTank::Rotate()
+{
+	AddActorLocalRotation(RotationDirection, true);
+}
---
+++ ToonTanks/Source/ToonTanks/Pawns/PawnTank.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTank.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTank : public APawnBase
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components/PawnTank", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components/PawnTank", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+
+	FVector MoveDirection;
+	FQuat RotationDirection;
+
+	void CalculateMoveInput(float Value);
+
+	void CalculateRotateInput(float Value);
+
+	void Move();
+
+	void Rotate();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tank/Movement", meta = (AllowPrivateAccess = "true"))
+	float MoveSpeed = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tank/Movement", meta = (AllowPrivateAccess = "true"))
+	float RotateSpeed = 100.0f;
+
+public:
+	// Sets default values for this pawn's properties
+	APawnTank();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+
+};
