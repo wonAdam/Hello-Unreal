aec0b7a841de022672af9e4ceef825a8f7ed3e65
원동현 <ehdgus5500@gmail.com>
2021-10-06 19:12:16 +0900

    172 ~ 173

---
+++ ToonTanks/Source/ToonTanks/ProjectileBase.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+class UProjectileMovementComponent;
+
+UCLASS()
+class TOONTANKS_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ProjectileBase/Components", meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ProjectileBase/Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* ProjectileMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectileBase/Damage")
+	TSubclassOf<UDamageType> DamageType;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ProjectileBase/Movement", meta = (AllowPrivateAccess = "true"))
+	float MovementSpeed = 1300.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "ProjectileBase/Damage", meta = (AllowPrivateAccess = "true"))
+	float Damage = 50.0f;
+
+public:	
+	// Sets default values for this actor's properties
+	AProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
